ad9e54eaae5a5ad9928be4a7064dba5ce570aee5
pavlovetor <pavlove.filip@gmail.com>
2025-02-13 22:44:37 +0100

    sentences

--- main.cpp
+++ main.cpp
@@ -2,30 +2,60 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+
+
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 800;
+
+void read_questions(std::string questions_path, 
+                    std::vector<std::string>& questions) {
+    std::ifstream file(questions_path);
+    std::string line;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            std::cout << line << std::endl;
+            questions.push_back(line);
+        }
+
+        file.close();
+    }
+    else {
+        std::cerr << "Unable to open file!" << std::endl;
+    }
+
+}
+
+void draw_questions(std::vector<std::string>& questions, const int index) {
+    DrawText(questions.at(index).c_str(), 20, 20, 30, RED);
+}
+
 
 int main(void) {
     // Initialization
-    const int SCREEN_WIDTH = 1000;
-    const int SCREEN_HEIGHT = 800;
     int FPS = 60;
-
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "YAT");
     SetTargetFPS(FPS);
 
-    std::vector<std::string> questions {
-        "aa",
-        "bb",
-        "cc"
-    };
+    // this should have class of its own
+    int idx = 0;
+    std::string questions_path = "../questions.txt";
+    std::vector<std::string> questions;
+    read_questions(questions_path, questions);
 
     while (!WindowShouldClose()) {
         // Event Handling
-
+        if (IsKeyPressed(KEY_ENTER)) {
+            idx++;
+        }
         // Update
+
 
         // Draw
         BeginDrawing();
         ClearBackground(WHITE);
+        draw_questions(questions, idx);
         EndDrawing();
     }
 
